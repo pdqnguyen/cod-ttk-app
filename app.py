df40d81e3e18b4353834f16453da3b5b0a2e298b
pdqnguyen <philnguyen94@gmail.com>
2021-05-06 13:33:17 -0700

    remove import

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
-import plotly.graph_objects as go
 
 import utils
 from truegamedata import get_weapons_data
